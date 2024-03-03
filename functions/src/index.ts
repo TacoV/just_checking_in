334e5ae88d8e6a0da9ea27cce1cf8cc72fcd1e4c
Taco Vader <taco.vader@crisp-e.com>
2024-03-03 20:39:44 +0100

    Some tests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -23,3 +23,15 @@ const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addmessage = onRequest(async (req, res) => {
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await getFirestore()
+        .collection("messages")
+        .add({query: req.params });
+    // Send back a message that we've successfully written the message
+    res.json({result: req.query });
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
