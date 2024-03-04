ad624f52ba670d3d9a643d3c2b1e357a3b01dece
Taco Vader <taco.vader@crisp-e.com>
2024-03-04 22:05:14 +0100

    Minimal Failing Example

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,3 @@
-import * as express from "express";
-import {onRequest, Request} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
-
-initializeApp();
-
-const test = async (req:Request, res:express.Response) => {
-    
-    const writeResult = await getFirestore()
-        .collection("messages")
-        .add({query: req.query });
-
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-
-  };
-
-exports.addmessage = onRequest(test)
+import {Telegraf} from "telegraf";
+const bot = new Telegraf("123:ABC");
+console.log(bot);
