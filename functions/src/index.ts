1a560349572d913cf7df80255d4900cd2efe4b89
TacoV <TacoV@users.noreply.github.com>
2024-03-05 17:03:09 +0100

    Untested rewrite of main func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,24 @@
-import {Telegraf} from "telegraf";
-const bot = new Telegraf("123:ABC");
-console.log(bot);
+import * as express from "express";
+import {onRequest, Request} from "firebase-functions/v2/https";
+
+const { defineInt, defineString } = require('firebase-functions/params');
+
+const TELEGRAM_API_TOKEN = defineString('TELEGRAM_API_TOKEN');
+
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
+initializeApp();
+
+exports.saveMessageToDB = onRequest(
+    { TELEGRAM_API_TOKEN: TELEGRAM_API_TOKEN },
+    async (req:Request, res:express.Response) => {
+    
+        const writeResult = await getFirestore()
+            .collection("messages")
+            .add({query: req.query });
+    
+        res.json({result: `Message with ID: ${writeResult.id} added.`});
+    
+      }   
+)
