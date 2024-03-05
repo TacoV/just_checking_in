4ccbcd3bf5e6711cb43d18cbfc518512db8d82e2
TacoV <TacoV@users.noreply.github.com>
2024-03-05 15:42:58 +0100

    David hotline

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -25,8 +25,8 @@ module.exports = {
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": ["warn", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "indent": ["warn", 2],
   },
 };
