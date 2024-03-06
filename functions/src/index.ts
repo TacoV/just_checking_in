7dfc9db568eb787e7cfc168264ddb914234b0d6a
Taco Vader <taco.vader@crisp-e.com>
2024-03-06 22:01:43 +0100

    Simplify code for MVP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,27 @@
-import * as express from "express";
-import {onRequest, Request} from "firebase-functions/v2/https";
+import {onRequest} from "firebase-functions/v2/https";
 import {defineString} from "firebase-functions/params";
 import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+// import {getFirestore} from "firebase-admin/firestore";
+import {Telegraf} from "telegraf";
 
 const TELEGRAM_API_TOKEN = defineString("TELEGRAM_API_TOKEN");
 
 initializeApp();
 
-exports.saveMessageToDB = onRequest(
-  async (req:Request, res:express.Response) => {
-    const writeResult = await getFirestore()
-      .collection("messages")
-      .add({query: req.query});
+const bot = new Telegraf(TELEGRAM_API_TOKEN.value());
 
-    res.json({result: `Message with ID: ${writeResult.id} added. API TOKEN ${TELEGRAM_API_TOKEN.value()}`});
-  }
-);
+bot.start((ctx) => ctx.reply("Welcome"));
+
+/*
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+
+bot.command("hello", async (ctx) => {
+  const writeResult = await getFirestore()
+    .collection("messages")
+    .add({query: ctx.botInfo});
+  ctx.reply(`Message with ID: ${writeResult.id} added.`);
+});
+*/
+
+exports.telegram = onRequest(bot.webhookCallback());
