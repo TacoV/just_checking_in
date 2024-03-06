2f79d25dcc7754611550945ce8b669b1efbb92d4
TacoV <TacoV@users.noreply.github.com>
2024-03-06 19:48:38 +0000

    Use the API TOKEN settings correctly

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,19 @@
 import * as express from "express";
 import {onRequest, Request} from "firebase-functions/v2/https";
-
 import {defineString} from "firebase-functions/params";
-
-const TELEGRAM_API_TOKEN = defineString("TELEGRAM_API_TOKEN");
-
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
+
+const TELEGRAM_API_TOKEN = defineString("TELEGRAM_API_TOKEN");
 
 initializeApp();
 
 exports.saveMessageToDB = onRequest(
-  {TELEGRAM_API_TOKEN: TELEGRAM_API_TOKEN},
   async (req:Request, res:express.Response) => {
     const writeResult = await getFirestore()
       .collection("messages")
       .add({query: req.query});
 
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+    res.json({result: `Message with ID: ${writeResult.id} added. API TOKEN ${TELEGRAM_API_TOKEN.value()}`});
   }
 );
