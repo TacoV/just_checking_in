b198c4faf8da83b05a4b8c280facb03e8b28858c
Taco Vader <taco.vader@crisp-e.com>
2024-03-29 22:09:46 +0100

    Remove remains of copy-paste

--- functions/src/types/question.ts
+++ functions/src/types/question.ts
@@ -1,11 +1,5 @@
 import {Timestamp} from "firebase-admin/firestore";
 
-export interface oftenScheduleParams {
-  minutes: number
-}
-export interface dailyScheduleParams {
-  time: string
-}
 export interface question {
       status: "planned" | "askes" | "answered" | "dropped",
       timing: Timestamp,
