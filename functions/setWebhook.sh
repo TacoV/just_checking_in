3e103e91d946e3311f22e3002f69b4e504a0ab3a
Taco Vader <taco.vader@crisp-e.com>
2024-03-30 21:05:57 +0100

    Hide my URL in the repos

--- functions/setWebhook.sh
+++ functions/setWebhook.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source .env
-curl -d "url=https://telegram-fsab3xrgqq-uc.a.run.app" "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/setWebhook"
+if [ -z ${APP_URL+x} ]
+then
+    echo "Please set APP_URL in .env to your Firebase function URL, eg APP_URL=https://telegram-ckjew3asd1.a.run.app"
+else 
+    curl -d "url=${APP_URL}" "https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/setWebhook"
+fi
