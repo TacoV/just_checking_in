32c73978bc379388bf82867a97c78dd3d859ad0a
TacoV <TacoV@users.noreply.github.com>
2024-04-04 21:36:45 +0000

    Try to get the db util working

--- functions/src/telegram/listAll.ts
+++ functions/src/telegram/listAll.ts
@@ -1,8 +1,20 @@
+import {FirestoreDataConverter, getFirestore} from "firebase-admin/firestore";
 import {Context} from "telegraf";
-import db from "../utils/db";
+import {schedule} from "../types/schedule";
+
+const converter = (): FirestoreDataConverter<schedule> => ({
+  toFirestore:
+    (data:schedule) =>
+      data as FirebaseFirestore.DocumentData,
+  fromFirestore:
+    (snap: FirebaseFirestore.QueryDocumentSnapshot) =>
+      snap.data() as schedule,
+});
 
 export default async function listAll(ctx: Context) {
-  const mySchedules = await db.schedules
+  const mySchedules = await getFirestore()
+    .collection("schedules")
+    .withConverter(converter())
     .where("chat", "==", ctx.chat?.id)
     .get();
   mySchedules.forEach(async (doc) => {
@@ -10,13 +22,14 @@ export default async function listAll(ctx: Context) {
       doc.data().question.question);
   });
 
-  const myQuestions = await db.questions
+  const myQuestions = await getFirestore()
+    .collection("questions")
     .where("chat", "==", ctx.chat?.id)
     .where("status", "==", "planned")
     .get();
   myQuestions.forEach( async (doc) => {
     await ctx.reply(
-      doc.data().timing.toDate().toLocaleTimeString +
+      doc.data().timing.toDate().toLocaleTimeString() +
         doc.data().question.question );
   });
 }
--- functions/src/utils/db.ts
+++ functions/src/utils/db.ts
@@ -1,21 +1,22 @@
 // https://medium.com/swlh/using-firestore-with-typescript-65bd2a602945
 
-import {firestore} from "firebase-admin";
-
 import {question} from "../types/question";
 import {schedule} from "../types/schedule";
-import {FirestoreDataConverter} from "firebase-admin/firestore";
+import {FirestoreDataConverter, getFirestore} from "firebase-admin/firestore";
 
 const converter = <T>(): FirestoreDataConverter<T> => ({
-  toFirestore: (data:T) => data as FirebaseFirestore.DocumentData,
+  toFirestore:
+    (data:T) =>
+      data as FirebaseFirestore.DocumentData,
   fromFirestore:
     (snap: FirebaseFirestore.QueryDocumentSnapshot) =>
-    snap.data() as T,
+      snap.data() as T,
 });
 
-const dataPoint = <T>(collectionPath: string) => firestore()
-  .collection(collectionPath)
-  .withConverter(converter<T>());
+const dataPoint = <T>(collectionPath: string) =>
+  getFirestore()
+    .collection(collectionPath)
+    .withConverter(converter<T>());
 
 const db = {
   questions: dataPoint<question>("questions"),
