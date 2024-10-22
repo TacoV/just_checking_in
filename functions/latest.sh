5cb798035724f66a6f88d97e3a89d9f7b3c7546c
TacoV <TacoV@users.noreply.github.com>
2024-10-22 07:12:45 +0000

    Better eslint

---
+++ functions/latest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm i -g \
+    firebase-tools@latest
+
+npm i \
+	firebase-admin@latest \
+	firebase-functions@latest \
+	luxon@latest \
+	telegraf@latest
+
+npm i -D \
+	eslint@latest \
+	typescript-eslint \
+	@eslint/js@latest \
+	@stylistic/eslint-plugin@latest \
+	eslint-config-google@latest \
+	@types/luxon@latest
